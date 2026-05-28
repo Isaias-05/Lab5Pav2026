f48f1d0b75118abb4cddac9243b1f3f71b60e8a7
ezio815 <exio78@hotmail.com>
2026-05-28 17:24:59 -0300

    Conflictos resueltos

--- include/datatypes/DtMaterial.h
+++ include/datatypes/DtMaterial.h
@@ -1,23 +1,26 @@
-#ifndef DT_MATERIAL
-#define DT_MATERIAL
+#ifndef DTMATERIAL
+#define DTMATERIAL
 
 #include <string>
 using namespace std;
 
-class DtMaterial{
+class DtMaterial {
     private:
         string codigo;
         string titulo;
         int anioPublicacion;
-
     public:
-        DtMaterial(string, string, int);
-
+        DtMaterial();
+        DtMaterial(string codigo, string titulo, int anioPublicacion);
+        ~DtMaterial();
 
         string getCodigo();
+        void setCodigo(string codigo);
 
         string getTitulo();
+        void setTitulo(string titulo);
 
         int getAnioPublicacion();
+        void setAnioPublicacion(int anioPublicacion);
 };
 #endif
