016f1292aaf77467468fe9e71625ffba284e3626
Isaias-05 <151371346+Isaias-05@users.noreply.github.com>
2026-06-09 15:43:32 -0300

    Controlador Registrar Funcionario

--- include/icontroladores/IControladorRegistrarFuncionario.h
+++ include/icontroladores/IControladorRegistrarFuncionario.h
@@ -1,14 +1,15 @@
 #ifndef ICONTROLADORREGISTRARFUNCIONARIO
 #define ICONTROLADORREGISTRARFUNCIONARIO
 
-#include "DtLector.h"
+#include "DtFuncionario.h"
 #include <string>
 using std::string;
 
 class IControladorRegistrarFuncionario {
 public:
 	virtual ~IControladorRegistrarFuncionario() {}
-	virtual DtLector registrarFuncionario(string, string, int) = 0;
+	virtual DtFuncionario* registrarFuncionario(string, string, string, int) = 0;
+	virtual void altaFuncionario(DtFuncionario*) = 0;
 };
 
 #endif // ICONTROLADORREGISTRARFUNCIONARIO
